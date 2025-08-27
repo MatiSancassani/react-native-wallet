292d8b2a88320b3f2a582e0b1698506b8148a692
Sanca <matiassancassani@gmail.com>
2025-08-27 01:15:21 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-red-700">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-[2rem] text-white">Hello World!</Text>
+    </View>
+  );
+}
